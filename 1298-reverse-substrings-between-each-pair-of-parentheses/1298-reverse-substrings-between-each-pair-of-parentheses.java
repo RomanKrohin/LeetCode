1932b35b6e6ea273f4e41864f687a014912dada8
RomanPPiroman <114436841+RomanKrohin@users.noreply.github.com>
2024-07-11 12:57:07 +0300

    Time: 1 ms (99.49%), Space: 41.3 MB (90.75%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
@@ -3,16 +3,21 @@ import java.util.regex.Pattern;
 
 class Solution {
     public String reverseParentheses(String s) {
-        String regex = "\\([^()]*\\)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(s);
+        Stack<StringBuilder> stack = new Stack<>();
+        StringBuilder current = new StringBuilder();
 
-        while (matcher.find()) {
-            String tmp = matcher.group();
-            String reversed = new StringBuilder(tmp).reverse().toString().replace("(", "").replace(")", "");
-            s = s.replace(tmp, reversed);
-            matcher = pattern.matcher(s);
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(current);
+                current = new StringBuilder();
+            } else if (c == ')') {
+                current.reverse();
+                current = stack.pop().append(current);
+            } else {
+                current.append(c);
+            }
         }
-        return s;
+
+        return current.toString();
     }
 }

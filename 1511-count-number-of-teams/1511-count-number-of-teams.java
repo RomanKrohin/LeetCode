7cd8dd47461d798e8cefdeea6e27b292bb0bb3f7
RomanPPiroman <114436841+RomanKrohin@users.noreply.github.com>
2024-07-29 21:16:33 +0300

    Time: 19 ms (50.49%), Space: 42.4 MB (45.15%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,41 +1,39 @@
 class Solution {
     public int numTeams(int[] rating) {
+        int n = rating.length;
+        int[] lessFromLeft = new int[n];
+        int[] lessFromRight = new int[n];
+        int[] greaterFromLeft = new int[n];
+        int[] greaterFromRight = new int[n];
 
-        HashMap<Integer, ArrayList<Integer>> map_pravo = new HashMap<>();
-        HashMap<Integer, ArrayList<Integer>> map_levo = new HashMap<>();
-        int count =0;
-
-        for (int i=0; i < rating.length; i++){
-            ArrayList<Integer> list_pravo = map_pravo.getOrDefault(rating[i], new ArrayList<>());
-            ArrayList<Integer> list_levo = map_levo.getOrDefault(rating[i], new ArrayList<>());
-            for (int j=i+1; j < rating.length; j++){
-                if (rating[j]<rating[i]) list_pravo.add(rating[j]);
-            }
-
-            for (int k=i-1; k>-1; k--){
-                if (rating[k]<rating[i]) list_levo.add(rating[k]);
-            }
-            map_pravo.put(rating[i], list_pravo);
-            map_levo.put(rating[i], list_levo);
-        }
-        
-        for (int key: map_pravo.keySet()){
-            if (map_pravo.get(key).size()>0){
-                for (int tmp: map_pravo.get(key)){
-                    if (map_pravo.get(tmp).size()>0) count+= map_pravo.get(tmp).size();
+        // Count the number of elements less than and greater than rating[i] from the left
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (rating[j] < rating[i]) {
+                    lessFromLeft[i]++;
+                } else if (rating[j] > rating[i]) {
+                    greaterFromLeft[i]++;
                 }
             }
         }
 
-        for (int key: map_pravo.keySet()){
-            if (map_levo.get(key).size()>0){
-                for (int tmp: map_levo.get(key)){
-                    if (map_levo.get(tmp).size()>0) count+= map_levo.get(tmp).size();
+        // Count the number of elements less than and greater than rating[i] from the right
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (rating[j] < rating[i]) {
+                    lessFromRight[i]++;
+                } else if (rating[j] > rating[i]) {
+                    greaterFromRight[i]++;
                 }
             }
+        }
+
+        int count = 0;
+        // Count the number of valid teams
+        for (int i = 0; i < n; i++) {
+            count += lessFromLeft[i] * greaterFromRight[i] + greaterFromLeft[i] * lessFromRight[i];
         }
 
         return count;
     }
 }
-
